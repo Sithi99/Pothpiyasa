c413d2650da9ace5c701a771b1259cfb05c58da3
Sithija Wickramasinghe <sithijawickramasinghe1999@gmail.com>
2022-11-20 22:30:08 +0530

    Insert CRUD operations

--- private/core/model.php
+++ private/core/model.php
@@ -29,5 +29,36 @@ class Model extends Database
         return $this->query($query);
     }
 
+    public function insert($data) //Getting single record
+    {
+        //Getting all the keys of associative array($data)
+        $keys = array_keys($data);
+        $columns = implode(',', $keys);
+        $values = implode(',:', $keys);
+
+        $query = "INSERT INTO $this->table ($columns) values (':'.$values)";
+
+        //Here, supply query data seperately
+        return $this->query($query, $data);
+    }
+
+    public function update($id,$data) //Getting single record
+    {
+        
+        $query = "SELECT * FROM $this->table WHERE $column = :value";
+
+        //Here, supply query data seperately
+        return $this->query($query, ['value' => $value] );
+    }
+
+    public function delete($id) //Getting single record
+    {
+        
+        $query = "SELECT * FROM $this->table WHERE $column = :value";
+
+        //Here, supply query data seperately
+        return $this->query($query, ['value' => $value] );
+    }
+
 
 }

eff8e302cf94c606c65854c713304943c34e1f87
Sandali Kavishka <103349227+SandaliKavi99@users.noreply.github.com>
2022-12-15 13:58:50 +0530

    Add Include folder

---
+++ private/views/librarian/includes/footer.view.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+
+</body>
+<script src="<?=ROOT?>/js/librarian/nav.js"></script>
+<script src="<?=ROOT?>/js/includes/calendar.js"></script>
+<script src="<?=ROOT?>/js/librarian/addAuthor.js"></script>
+
+</html>

53f79738b02c7c0f73b215c8fa20e79e3c2af5c6
kasun00 <92900452+kasun00@users.noreply.github.com>
2022-12-22 23:34:07 +0530

    library Staff includes added

---
+++ private/views/libraryStaff/includes/header.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PothPiyasa</title>
+    <link rel="stylesheet" href="<?=ROOT?>/css/includes/header.css">
+    <link rel="stylesheet" href="<?=ROOT?>/css/includes/popup.css">
+    <link rel="stylesheet" href="<?=ROOT?>/css/libraryStaff/dashboardLibraryStaff.css">
+    <link rel="stylesheet" href="<?=ROOT?>/css/librarian/includes/nav.css">
+    <link rel="stylesheet" href="<?=ROOT?>/css/all.min.css">
+    <link rel="stylesheet" href="<?=ROOT?>/css/includes/calendar.css">
+
+
+</head>
+
+

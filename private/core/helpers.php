933dd24de6d3363e5ebb350a7c70af59b3f93ee3
kasun00 <92900452+kasun00@users.noreply.github.com>
2022-12-22 23:18:21 +0530

    Added helpers.php file

--- private/core/helpers.php
+++ private/core/helpers.php
@@ -72,6 +72,7 @@ function get_user_name($key,$value)
     }
 }
 
+//Sandali
 function get_staff_name($key,$value)
 {
     $user = new LibraryStaff();
@@ -83,7 +84,7 @@ function get_staff_name($key,$value)
          $member = new User();
         if($row1= $member->where('UserID',$value1)){
             $row1 = $row1[0];
-            return $row1->FirstName."".$row1->MidName." ".$row1->LastName;
+            return $row1->FirstName." ".$row1->LastName;
 
         }
        
@@ -126,6 +127,231 @@ function viewbooksforauthor($authorID)
         $data = $book->where("AuthorID",$authorID);
         return $data;
    }
+
+//get category name
+function get_BookCategory($key,$data){
+   
+    $category = new BookCategory();
+    $data = $category->where($key,$data);
+    //print_r($data);
+    return $data[0]->Name;
+    
+    
+}
+
+
+//get rack id
+function get_rackID($key,$data){
+    $rack = new Rack();
+    if($row = $rack->where($key,$data)){
+        return $row[0]->RackID;
+    }
+    else{
+        return "None";
+    }
+}
+
+// //get category id
+
+// function get_categoryID($key,$data){
+//     $category = new BookCategory();
+//     if($row = $category->where($key,$data)){
+//         return $row[0]->CategoryID;
+//     }
+//     else{
+//         return "None";
+//     }
+// }
+
+
+function display_categories(){
+    $category = new BookCategory();
+    $data = $category->findAll();
+    
+    return $data;
+                
+}
+
+function get_bookname($key,$data){
+    $book = new Book();
+    if($row = $book->where($key,$data)){
+        return $row[0]->Title;
+    }
+    else{
+        return "None";
+    }
+                
+}
+
+//get fine amount
+
+function get_fine($key1,$data1,$data2){
+    $book = new ReturnBook();
+    if($rows = $book->where($key1,$data1)){
+        $flag =0;
+
+        foreach ($rows as $row){
+            if(($row->BookID==$data2) && ($row->FineStatus==-1)){
+                $flag =1;
+                return $row->FineAmount;
+            }
+            
+
+        }
+        if($flag==0){
+            return "None";
+        }
+
+    }
+    else{
+            return "None2";
+        }
+
+            
+    
+                
+}
+
+
+   
+//kasun
+
+
+
+//get userID
+function get_userID($key,$data){
+    $staff = new LibraryStaff();
+    if($row = $staff->where($key,$data)){
+        return $row[0]->StaffID;
+    }
+    else{
+        return "None";
+    }
+}
+
+//get authorID
+function get_authorID($key,$data){
+    $author = new Author();
+    if($row = $author->where($key,$data)){
+        return $row[0]->AuthorID;
+    }
+    else{
+        return "None";
+    }
+}
+
+//get authorName
+function get_authorName($key,$data){
+    $author = new Author();
+    if($row = $author->where($key,$data)){
+        return $row[0]->Name;
+    }
+    else{
+        return "None";
+    }
+}
+
+//get Vendor Name
+function get_VendorName($key,$data){
+    $vendor = new Vendor();
+    if($row = $vendor->where($key,$data)){
+        return $row[0]->Name;
+    }
+    else{
+        return "None";
+    }
+}
+
+//get donor Name
+function get_DonorName($key,$data){
+    $donor = new Donor();
+    if($row = $donor->where($key,$data)){
+        return $row[0]->Name;
+    }
+    else{
+        return "None";
+    }
+}
+
+//get staff Name
+function get_StaffName($key,$data){
+    $staff = new LibraryStaff();
+    if($row = $staff->where($key,$data)){
+        $userID= $row[0]->UserID;
+    }
+    $user = new User();
+    if($row2 = $user->where("UserID",$userID)){
+       return $row2[0]->FirstName." ".$row2[0]->LastName;
+    }
+    else{
+        return "None";
+    }
+}
+
+//get category name
+function get_CategoryName($key,$data){
+    $category = new BookCategory();
+    if($row = $category->where($key,$data)){
+        return $row[0]->Name;
+    }
+    else{
+        return "None";
+    }
+}
+
+
+//get category id
+function get_categoryID($key,$data){
+    $category = new BookCategory();
+    if($row = $category->where($key,$data)){
+        return $row[0]->CategoryID;
+    }
+    else{
+        return "None";
+    }
+}
+
+// get delete url
+// function set_DeleteUrl($link){
+//     $GLOBALS['deleteLink'] = $link;
+// }
+
+// function get_DeleteUrl(){
+//     return $GLOBALS['deleteLink'];
+// }
+
+
+function get_selectedVendorDonorName($vendorID,$donorID){
+if(!empty($vendorID)){
+    $vendor = new Vendor();
+    if($row = $vendor->where("VendorID",$vendorID)){
+        return $row[0]->Name;
+    }
+    else{
+        return "None";
+    }
+}
+else if(!empty($donorID)){
+    $donor = new Donor();
+    if($row = $donor->where("DonorID",$donorID)){
+        return $row[0]->Name;
+    }
+    else{
+        return "None";
+    } 
+}
+}
+
+//select default button clicked
+function select_redioButton($value)
+{
+
+    if (!empty($value)) {
+        return "checked";
+    }
+    return "";
+    
+}
 
 
  
